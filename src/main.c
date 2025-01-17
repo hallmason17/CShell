24f2df2f7419e20181071c66d70829cfd1ddaca0
hallmason17 <hallmason17@gmail.com>
2025-01-17 12:22:02 -0500

    remove type command due to memory bug

--- src/main.c
+++ src/main.c
@@ -7,48 +7,9 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define PATH_MAX 4096
-#define NAME_MAX 255
-#define MAX_PATH_ENTRIES 250 * PATH_MAX
-#define MAX_FILE_NAME_SIZE PATH_MAX + NAME_MAX
-
-static char *builtins[3] = {"type", "echo", "cd"};
-
-/**
- * Returns true if command is in given path. Writes dir and filename into loc
- * param.
- * @param loc buffer to write dir/filename into
- * @param path_dirs path array
- * @param num_dirs entries in path
- * @param command command to search
- * @return true if found in path, false otherwise. writes path into loc.
- */
-bool get_cmd_from_path(char *loc, char **path_dirs, size_t num_dirs,
-                       char *command) {
-    char cmdexe[50];
-    struct dirent *pdirent;
-    snprintf(cmdexe, strlen(command) + 5, "%s.%s", command, "exe");
-    for (int j = 0; j < num_dirs; ++j) {
-        DIR *pdir = opendir(path_dirs[j]);
-        if (pdir) {
-            while ((pdirent = readdir(pdir))) {
-                if (strcmp(command, pdirent->d_name) == 0 ||
-                    strcmp(cmdexe, pdirent->d_name) == 0) {
-                    snprintf(loc,
-                             strlen(path_dirs[j]) + strlen(pdirent->d_name) + 2,
-                             "%s/%s", path_dirs[j], pdirent->d_name);
-                    return true;
-                }
-            }
-            closedir(pdir);
-        }
-    }
-    return false;
-}
-
 char *read_from_stdin() {
-    char *line;
-    size_t linelen;
+    char *line = NULL;
+    size_t linelen = 0;
     if (getline(&line, &linelen, stdin) == -1) {
         perror("csh");
         exit(1);
@@ -57,7 +18,7 @@ char *read_from_stdin() {
 }
 
 char **split_line(char *str, char *delim) {
-    char **split = malloc(1024 * sizeof(char));
+    char **split = calloc(1024 * sizeof(char *), 1);
     char *tok = strtok(str, delim);
     int i = 0;
     while (tok) {
@@ -65,12 +26,13 @@ char **split_line(char *str, char *delim) {
         ++i;
         tok = strtok(NULL, delim);
     }
+
     return split;
 }
 
 void exec_program_with_args(char **input) {
     pid_t pid;
-    int pstatus;
+    int pstatus = 0;
     pid = fork();
     if (pid == -1) {
         perror("csh");
@@ -78,6 +40,7 @@ void exec_program_with_args(char **input) {
         if (execvp(input[0], input) == -1) {
             perror("csh");
         }
+        exit(EXIT_FAILURE);
     } else {
         do {
             waitpid(pid, &pstatus, WUNTRACED);
@@ -85,50 +48,26 @@ void exec_program_with_args(char **input) {
     }
 }
 
-void type(char *cmd) {
-    for (int i = 0; i < 3; i++) {
-        if (strcmp(builtins[i], cmd) == 0) {
-            printf("%s: is a shell builtin\n", cmd);
-            return;
-        }
-    }
-    char *pathenv = getenv("PATH");
-
-    char *path_dirs[MAX_PATH_ENTRIES] = {0};
-    char *saveptr;
-    char *delim = ":";
-    char *tok = strtok_r(pathenv, delim, &saveptr);
-    int i = 0;
-    while (tok) {
-        path_dirs[i] = tok;
-        ++i;
-        tok = strtok_r(NULL, delim, &saveptr);
-    }
-    char cmdloc[MAX_FILE_NAME_SIZE];
-    int found = get_cmd_from_path(cmdloc, path_dirs, i, cmd);
-    if (found) {
-        printf("%s is %s\n", cmd, cmdloc);
-    } else {
-        fprintf(stderr, "csh: Command not found: %s\n", cmd);
-    }
-}
-
 int main(int argc, char *argv[]) {
-    char *line;
-    char **input;
     while (1) {
+        char *line;
+        char **input;
+
         printf("$ ");
+
         line = read_from_stdin();
         input = split_line(line, " \n");
-        if (strcmp("type", input[0]) == 0) {
-            type(input[1]);
-            continue;
+
+        if (strcmp("exit", input[0]) == 0) {
+            return EXIT_SUCCESS;
+        } else {
+            exec_program_with_args(input);
         }
-        exec_program_with_args(input);
+
         fflush(stdout);
         free(line);
         free(input);
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
